d30f4cdd9a3bffb02cf5c8d72d6742646a692de1
Argel Cavallini <argel@avantdigital.com.br>
2021-06-21 23:17:40 -0300

    AULA 02

---
+++ src/entities/User.ts
@@ -0,0 +1,32 @@
+import {Entity, PrimaryColumn,Column,CreateDateColumn,UpdateDateColumn} from "typeorm";
+import { v4 as uuid } from "uuid";
+
+@Entity("users") //NOME DA TABELA
+class User {
+
+  @PrimaryColumn()
+  readonly id:string;
+
+  @Column()
+  name:string;
+
+  @Column()
+  email:string;
+
+  @Column()
+  admin:boolean;
+
+  @CreateDateColumn()
+  creaated_at:Date;
+
+  @UpdateDateColumn()
+  updated_at:Date;
+
+  constructor(){
+    if(!this.id){
+      this.id = uuid();
+    }
+  }
+}
+
+export{User};
