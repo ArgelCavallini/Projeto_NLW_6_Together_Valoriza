fa502207c61732d05cd726950fc8a94e25921a2e
Argel Cavallini <argel_095@hotmail.com>
2021-07-26 21:41:15 -0300

    ajustee login

--- src/modules/user/infra/http/routes/user.routes.ts
+++ src/modules/user/infra/http/routes/user.routes.ts
@@ -3,13 +3,18 @@ import { Router } from 'express';
 import { CreateUserController } from "../controllers/CreateUserController";
 import { ListUserController } from "../controllers/ListUserController";
 import { ensureAthenticated } from "../../../../../middlewares/ensureAthenticated";
+import { AuthenticateUserController } from "../controllers/AuthenticateUserController";
+
 
 const router = Router();
 
 const createUserController = new CreateUserController();
 const listUserController = new ListUserController();
+const authenticateUserController = new AuthenticateUserController();
+
 
 router.post("/users", createUserController.handle);
 router.get("/users/", ensureAthenticated,listUserController.handle);
+router.post("/login", authenticateUserController.handle);
 
 export default router;
